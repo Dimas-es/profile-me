fe2162c8990e2ad055028e5385b0aa0c638d2400
Dimas-es <evosdimm@gmail.com>
2025-06-07 23:08:13 +0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/profile-me' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nama-repository-anda/' : '',
 }
 
 export default nextConfig
